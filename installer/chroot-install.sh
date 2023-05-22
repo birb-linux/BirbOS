ae69540900e991235e1d29a2c89bdc5cafd90518
toasterbirb <git@toasterbirb.com>
2023-05-22 23:33:45 +0300

    Avoid birb taking over some X11 font dirs

--- installer/chroot-install.sh
+++ installer/chroot-install.sh
@@ -26,6 +26,9 @@ mkdir -pv /var/lib/{color,misc,locate}
 
 mkdir -pv /usr/share/fonts/TTF
 touch /usr/share/fonts/TTF/.birb_symlink_lock
+mkdir -pv /usr/share/fonts/X11/{misc,util}
+touch /usr/share/fonts/X11/misc/.birb_symlink_lock,util
+touch /usr/share/fonts/X11/util/.birb_symlink_lock,util
 
 mkdir -pv /usr/share/applications
 touch /usr/share/applications/.birb_symlink_lock
