aa5466f96689d843abf61837a48ee4ea2402f889
Toasterbirb <git@toasterbirb.com>
2024-11-14 01:35:09 +0200

    Fix some shellcheck issues in the source downloading script + reformat an if-statement

--- installer/bootstrap-scripts/download_sources.sh
+++ installer/bootstrap-scripts/download_sources.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+if [ -z "$LFS" ]
+then
+	echo "The LFS environment variable is not set"
+	exit 1
+fi
+
 LFS_SRC_PREFIX="$LFS/sources"
 ERRORS=""
 
 download()
 {
-	URL="$(echo $1 | cut -d';' -f1)"
-	MD5_SUM="$(echo $1 | cut -d';' -f2)"
+	URL="$(echo "$1" | cut -d';' -f1)"
+	MD5_SUM="$(echo "$1" | cut -d';' -f2)"
 
-	FILE_NAME="$(basename $URL)"
+	FILE_NAME="$(basename "$URL")"
 
 	# Check for existing files
-	[ -f "$LFS_SRC_PREFIX/$FILE_NAME" ] && { [ "$(md5sum $LFS_SRC_PREFIX/$FILE_NAME | cut -d' ' -f1)" == "$MD5_SUM" ] && echo "$FILE_NAME found" && return || rm -fv "$LFS_SRC_PREFIX/$FILE_NAME"; }
+	[ -f "$LFS_SRC_PREFIX/$FILE_NAME" ] && {
+		if [ "$(md5sum "$LFS_SRC_PREFIX/$FILE_NAME" | cut -d' ' -f1)" == "$MD5_SUM" ]
+		then
+			echo "$FILE_NAME found"
+		else
+			rm -fv "$LFS_SRC_PREFIX/$FILE_NAME";
+		fi
+	}
 
-	wget --directory-prefix=$LFS_SRC_PREFIX $URL
+	wget --directory-prefix="$LFS_SRC_PREFIX" "$URL"
 
-	DOWNLOAD_MD5_SUM="$(md5sum $LFS_SRC_PREFIX/$FILE_NAME | cut -d' ' -f1)"
+	DOWNLOAD_MD5_SUM="$(md5sum "$LFS_SRC_PREFIX/$FILE_NAME" | cut -d' ' -f1)"
 	[ "$DOWNLOAD_MD5_SUM" != "$MD5_SUM" ] && echo "MD5 mismatch with $FILE_NAME" && ERRORS="YES" && exit 1
 }
 
 # Source urls and md5sums
+#shellcheck disable=SC1091
 source ./bootstrap-scripts/packages.sh
 
 # Get the list of variable names
@@ -29,11 +43,11 @@ PACKAGE_LIST="$(awk -F'=' '/^[A-Za-z0-9]/ {print $1}' ./bootstrap-scripts/packag
 for i in $PACKAGE_LIST
 do
 	echo "Downloading: $i"
-	download ${!i}
+	download "${!i}"
 done
 echo "Finished downloading!"
 
 # Make sure that all of the source files are owned by root
-chown root:root $LFS/sources/*
+chown root:root "$LFS"/sources/*
 
 [ -n "$ERRORS" ] && exit 1 || exit 0
