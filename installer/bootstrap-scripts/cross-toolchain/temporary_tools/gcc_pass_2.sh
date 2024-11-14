c473d42fd4406341591c484743b600be9284bf2b
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:59:55 +0200

    Update gcc_pass_2

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/gcc_pass_2.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/gcc_pass_2.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/gcc-pass2.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$gcc"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 # Extract some packages that aren't yet compiled into the system
-tar -xf ../mpfr-4.2.0.tar.xz
-mv -v mpfr-4.2.0 mpfr
-tar -xf ../gmp-6.2.1.tar.xz
-mv -v gmp-6.2.1 gmp
+tar -xf ../mpfr-4.2.1.tar.xz
+mv -v mpfr-4.2.1 mpfr
+tar -xf ../gmp-6.3.0.tar.xz
+mv -v gmp-6.3.0 gmp
 tar -xf ../mpc-1.3.1.tar.gz
 mv -v mpc-1.3.1 mpc
 
+# shellcheck disable=SC2016
 sed -e '/m64=/s/lib64/lib/' \
     -e '/m32=/s/m32=.*/m32=..\/lib32$(call if_multiarch,:i386-linux-gnu)/' \
     -i.orig gcc/config/i386/t-linux64
@@ -28,31 +30,30 @@ cd       build
 
 mlist=m64,m32
 ../configure                                       \
-    --build=$(../config.guess)                     \
-    --host=$LFS_TGT                                \
-    --target=$LFS_TGT                              \
-    LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc      \
+    --build="$(../config.guess)"                   \
+    --host="$LFS_TGT"                              \
+    --target="$LFS_TGT"                            \
+    LDFLAGS_FOR_TARGET=-L"$PWD/$LFS_TGT/libgcc"    \
     --prefix=/usr                                  \
-    --with-build-sysroot=$LFS                      \
+    --with-build-sysroot="$LFS"                    \
     --enable-default-pie                           \
     --enable-default-ssp                           \
-    --enable-initfini-array                        \
     --disable-nls                                  \
     --enable-multilib --with-multilib-list=$mlist  \
-    --disable-decimal-float                        \
     --disable-libatomic                            \
     --disable-libgomp                              \
     --disable-libquadmath                          \
+    --disable-libsanitizer                         \
     --disable-libssp                               \
     --disable-libvtv                               \
     --enable-languages=c,c++
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
 # Symlink cc to gcc
-ln -sv gcc $LFS/usr/bin/cc
+ln -sv gcc "$LFS/usr/bin/cc"
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
