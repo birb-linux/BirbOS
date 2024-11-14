86dc761321c85f22f83e108d78f10ece7bae596c
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:41:17 +0200

    Update coreutils

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/coreutils.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/coreutils.sh
@@ -1,28 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/coreutils.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$coreutils"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
-./configure --prefix=/usr                     \
-            --host=$LFS_TGT                   \
-            --build=$(build-aux/config.guess) \
-            --enable-install-program=hostname \
+./configure --prefix=/usr                       \
+            --host="$LFS_TGT"                   \
+            --build="$(build-aux/config.guess)" \
+            --enable-install-program=hostname   \
             --enable-no-install-program=kill,uptime
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
 # Move programs to their final expected locations
-mv -v $LFS/usr/bin/chroot              $LFS/usr/sbin
-mkdir -pv $LFS/usr/share/man/man8
-mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
-sed -i 's/"1"/"8"/'                    $LFS/usr/share/man/man8/chroot.8
+mv -v "$LFS/usr/bin/chroot"              "$LFS/usr/sbin"
+mkdir -pv "$LFS/usr/share/man/man8"
+mv -v "$LFS/usr/share/man/man1/chroot.1" "$LFS/usr/share/man/man8/chroot.8"
+sed -i 's/"1"/"8"/'                    "$LFS/usr/share/man/man8/chroot.8"
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
