ae742f359897dccf4774e2fa1bd12c40627d2f46
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:52:57 +0200

    Update xz

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/xz.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/xz.sh
@@ -1,25 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/xz.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$xz"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
-./configure --prefix=/usr                     \
-            --host=$LFS_TGT                   \
-            --build=$(build-aux/config.guess) \
-            --disable-static                  \
-            --docdir=/usr/share/doc/xz-5.4.2
+./configure --prefix=/usr                       \
+            --host="$LFS_TGT"                   \
+            --build="$(build-aux/config.guess)" \
+            --disable-static                    \
+            --docdir=/usr/share/doc/xz-5.6.3
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
 # Remove the libtool archive, because it is harmful for cross compilation
-rm -v $LFS/usr/lib/liblzma.la
+rm -v "$LFS/usr/lib/liblzma.la"
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
