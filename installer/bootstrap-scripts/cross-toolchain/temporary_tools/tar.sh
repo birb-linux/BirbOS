f8b9d7c2b4e227aebec4fc62959608f77318db20
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:52:00 +0200

    Update tar

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/tar.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/tar.sh
@@ -1,20 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/tar.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$tar"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
-./configure --prefix=/usr                     \
-            --host=$LFS_TGT                   \
-            --build=$(build-aux/config.guess)
+./configure --prefix=/usr     \
+            --host="$LFS_TGT" \
+            --build="$(build-aux/config.guess)"
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
