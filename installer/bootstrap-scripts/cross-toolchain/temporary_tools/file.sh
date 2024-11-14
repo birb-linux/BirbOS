264ad1339207ac54fa24bf92775997af0d7501ef
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:44:07 +0200

    Update file

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/file.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/file.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/file.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$file"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 # Create a temporary copy of the host `file` command
 mkdir build
@@ -18,14 +19,14 @@ pushd build
   make
 popd
 
-./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
+./configure --prefix=/usr --host="$LFS_TGT" --build="$(./config.guess)"
 
-make -j$(nproc) FILE_COMPILE=$(pwd)/build/src/file
+make -j "$(nproc)" FILE_COMPILE="$(pwd)/build/src/file"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
 # Remove the libtool archive file, because its harmful to the cross compilation
-rm -v $LFS/usr/lib/libmagic.la
+rm -v "$LFS/usr/lib/libmagic.la"
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
