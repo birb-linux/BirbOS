aa0e0114e5afe3b960758eb3dd19fe61fa085dd3
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:45:17 +0200

    Update findutils

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/findutils.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/findutils.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/findutils.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$findutils"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 ./configure --prefix=/usr                   \
             --localstatedir=/var/lib/locate \
-            --host=$LFS_TGT                 \
-            --build=$(build-aux/config.guess)
+            --host="$LFS_TGT"               \
+            --build="$(build-aux/config.guess)"
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
