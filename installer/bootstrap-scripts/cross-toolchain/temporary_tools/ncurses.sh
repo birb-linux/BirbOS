f6ba3c24debd3712ab1db19e5e217bd187e31a3b
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:37:43 +0200

    Update ncurses

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/ncurses.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/ncurses.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/ncurses.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$ncurses"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 # Make sure that gawk is found
 sed -i s/mawk// configure
@@ -21,8 +22,8 @@ popd
 
 # Prapare for compilation
 ./configure --prefix=/usr                \
-            --host=$LFS_TGT              \
-            --build=$(./config.guess)    \
+            --host="$LFS_TGT"            \
+            --build="$(./config.guess)"  \
             --mandir=/usr/share/man      \
             --with-manpage-format=normal \
             --with-shared                \
@@ -31,38 +32,39 @@ popd
             --without-debug              \
             --without-ada                \
             --disable-stripping          \
-            --enable-widec
+            AWK=gawk
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
-echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
+make DESTDIR="$LFS" TIC_PATH="$(pwd)/build/progs/tic" install
+ln -sv libncursesw.so "$LFS/usr/lib/libncurses.so"
+sed -e 's/^#if.*XOPEN.*$/#if 1/' \
+    -i "$LFS/usr/include/curses.h"
 
 # Building the 32bit version
 make distclean
 
-CC="$LFS_TGT-gcc -m32"              \
-CXX="$LFS_TGT-g++ -m32"             \
-./configure --prefix=/usr           \
-            --host=$LFS_TGT32       \
-            --build=$(./config.guess)    \
-            --libdir=/usr/lib32     \
-            --mandir=/usr/share/man \
-            --with-shared           \
-            --without-normal        \
-            --with-cxx-shared       \
-            --without-debug         \
-            --without-ada           \
-            --disable-stripping     \
-            --enable-widec
+CC="$LFS_TGT-gcc -m32"                  \
+CXX="$LFS_TGT-g++ -m32"                 \
+./configure --prefix=/usr               \
+            --host="$LFS_TGT32"         \
+            --build="$(./config.guess)" \
+            --libdir=/usr/lib32         \
+            --mandir=/usr/share/man     \
+            --with-shared               \
+            --without-normal            \
+            --with-cxx-shared           \
+            --without-debug             \
+            --without-ada               \
+            --disable-stripping
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$PWD/DESTDIR TIC_PATH=$(pwd)/build/progs/tic install
-ln -s libncursesw.so DESTDIR/usr/lib32/libcursesw.so
-cp -Rv DESTDIR/usr/lib32/* $LFS/usr/lib32
+make DESTDIR="$PWD/DESTDIR" TIC_PATH="$(pwd)/build/progs/tic" install
+ln -sv libncursesw.so DESTDIR/usr/lib32/libncurses.so
+cp -Rv DESTDIR/usr/lib32/* "$LFS/usr/lib32"
 rm -rf DESTDIR
 
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
