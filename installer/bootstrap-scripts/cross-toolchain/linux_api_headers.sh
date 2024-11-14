4dc9a29e33c6f59e0fa467ace941aba0a665c6ec
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:20:54 +0200

    Update linux_api_headers

--- installer/bootstrap-scripts/cross-toolchain/linux_api_headers.sh
+++ installer/bootstrap-scripts/cross-toolchain/linux_api_headers.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter05/linux-headers.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$linux"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 make mrproper
 
 make headers
 find usr/include -type f ! -name '*.h' -delete
-cp -rv usr/include $LFS/usr
+cp -rv usr/include "$LFS/usr"
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
