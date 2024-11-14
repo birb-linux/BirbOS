d7e26b83eee1518bdeeb0d0110730b2ea72d87a6
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:07:31 +0200

    Fix some shellcheck errors in the bison install script

--- installer/bootstrap-scripts/chroot-temptools/bison.sh
+++ installer/bootstrap-scripts/chroot-temptools/bison.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter07/bison.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$bison"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
 ./configure --prefix=/usr \
             --docdir=/usr/share/doc/bison-3.8.2
 
-make -j$(nproc)
+make -j "$(nproc)"
 
 make install
 
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
-~/bootstrap-scripts/installation_progress_manager.sh add $0
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
