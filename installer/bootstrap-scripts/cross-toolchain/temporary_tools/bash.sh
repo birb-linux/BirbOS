3aa9cbe2538bc54d1de5ae8f8cc24ce34c5528e8
Toasterbirb <git@toasterbirb.com>
2024-11-14 02:39:34 +0200

    Update bash

--- installer/bootstrap-scripts/cross-toolchain/temporary_tools/bash.sh
+++ installer/bootstrap-scripts/cross-toolchain/temporary_tools/bash.sh
@@ -1,27 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC1090,SC2154
 # https://www.linuxfromscratch.org/~thomas/multilib/chapter06/bash.html
 set -e
-~/bootstrap-scripts/installation_progress_manager.sh check $0 && exit 0
+~/bootstrap-scripts/installation_progress_manager.sh check "$0" && exit 0
 source ~/bootstrap-scripts/packages.sh
 
 PACKAGE="$bash"
 
-. ~/bootstrap-scripts/prepare_sources.sh $PACKAGE
+. ~/bootstrap-scripts/prepare_sources.sh "$PACKAGE"
 
-./configure --prefix=/usr                      \
-            --build=$(sh support/config.guess) \
-            --host=$LFS_TGT                    \
+./configure --prefix=/usr                        \
+            --build="$(sh support/config.guess)" \
+            --host="$LFS_TGT"                    \
             --without-bash-malloc
 
-make -j$(nproc)
+make -j "$(nproc)"
 
-make DESTDIR=$LFS install
+make DESTDIR="$LFS" install
 
 # Set bash as the /bin/sh symlink
-ln -sv bash $LFS/bin/sh
+ln -sv bash "$LFS/bin/sh"
 
 # There is some permission issue with removing the bash source files
 # so we need to increment the progress before removing them, because
 # that would trigger `set -e` and the progress wouldn't get incremented
-~/bootstrap-scripts/installation_progress_manager.sh add $0
-~/bootstrap-scripts/remove_sources.sh $PACKAGE
+~/bootstrap-scripts/installation_progress_manager.sh add "$0"
+~/bootstrap-scripts/remove_sources.sh "$PACKAGE"
